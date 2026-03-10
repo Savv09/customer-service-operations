95087fd3ef326bf14529cdbd86a68183b1fafec5
Savio <saverio.centola18@gmail.com>
2026-03-10 11:58:12 +0100

    added router-outlet to app.component and setted first basic route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Login } from './core/auth/login/login';
+import { DashboradLayout } from './core/layout/dashborad-layout/dashborad-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: Login },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./core/layout/dashborad-layout/dashborad-layout').then((c) => c.DashboradLayout),
+  },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+];
