5779e3630ee6835243bce2b5b894309985d0db2d
Savio <saverio.centola18@gmail.com>
2026-03-10 11:20:27 +0100

    Setting of environments with firebase api and initial implementation of auth logic

---
+++ src/app/core/auth/auth.service.ts
@@ -0,0 +1,32 @@
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+
+export interface AuthResponse {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private APIKey = environment.firebaseConfig.apiKey;
+
+  http = inject(HttpClient);
+
+  login(email: string, password: string) {
+    this.http.post<AuthResponse>(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${this.APIKey}`,
+      {
+        email,
+        password,
+        returnSecureToken: true,
+      },
+    );
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAr4AUVdlh_JNRuifsw7nTbWY8FCumYTjQ',
+    authDomain: 'customer-service-operation.firebaseapp.com',
+    projectId: 'customer-service-operation',
+    storageBucket: 'customer-service-operation.firebasestorage.app',
+    messagingSenderId: '421161495443',
+    appId: '1:421161495443:web:99b9ea9f912b3374899666',
+  },
+};
