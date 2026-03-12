65e8e2da36fccb24b530e2fc851b296afb157f9f
Savio <saverio.centola18@gmail.com>
2026-03-12 12:43:08 +0100

    removed getHeadersFunction thanks to the setHeaderInterceptor

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -1,10 +1,12 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { BASE_URL } from '../contsants/base.const';
+import { HttpClient } from '@angular/common/http';
+
 import { map, Observable } from 'rxjs';
+
 import { formatUserFromFirestore } from '../utils/api-formatter';
 import { UserFromApi } from '../models/responses-from-api.model';
 import { User } from '../models/user.model';
+import { BASE_URL } from '../contsants/base.const';
 
 @Injectable({
   providedIn: 'root',
@@ -22,20 +24,11 @@ export class UserService {
     return usersUrl;
   }
 
-  private getHeaders() {
-    return new HttpHeaders({
-      Authorization: `Bearer ${localStorage.getItem('token')}`,
-    });
-  }
-
   getUserList() {}
 
   getUser(userId: string): Observable<User> {
     const url = this.getUsersUrl(userId);
-    const headers = this.getHeaders();
 
-    return this.http
-      .get<UserFromApi>(url, { headers: headers })
-      .pipe(map((res) => formatUserFromFirestore(res)));
+    return this.http.get<UserFromApi>(url).pipe(map((res) => formatUserFromFirestore(res)));
   }
 }
