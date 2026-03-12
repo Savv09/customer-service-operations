7752a06fa33eac118bcf9ce21d81c5c7c73a5789
Savio <saverio.centola18@gmail.com>
2026-03-12 16:30:50 +0100

    added guard that checks if the user is authenticated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Login } from './core/auth/login/login';
 import { DashboradLayout } from './core/layout/dashborad-layout/dashborad-layout';
+import { authGuard } from './core/guards/auth-guard';
 
 export const routes: Routes = [
   { path: 'login', component: Login },
@@ -8,6 +9,7 @@ export const routes: Routes = [
     path: 'dashboard',
     loadComponent: () =>
       import('./core/layout/dashborad-layout/dashborad-layout').then((c) => c.DashboradLayout),
+    canActivate: [authGuard],
   },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
