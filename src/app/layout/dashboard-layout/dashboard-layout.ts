8e7902d5baba3374f8bbcdc022493f382441194b
Savio <saverio.centola18@gmail.com>
2026-03-24 11:35:41 +0100

    finished basic layout

--- src/app/layout/dashboard-layout/dashboard-layout.ts
+++ src/app/layout/dashboard-layout/dashboard-layout.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 
 import { Sidebar } from '../../shared/sidebar/sidebar';
 import { Header } from '../../shared/header/header';
+import { RouterOutlet } from '../../../../node_modules/@angular/router/types/_router_module-chunk';
 
 @Component({
   selector: 'app-dashborad-layout',
-  imports: [Sidebar, Header],
+  imports: [Sidebar, Header, RouterOutlet],
   templateUrl: './dashboard-layout.html',
   styleUrl: './dashboard-layout.css',
 })
