5e072c4ae2d495f76aa26faa4d71614594ec9748
Savio <saverio.centola18@gmail.com>
2026-03-25 11:46:54 +0100

    added button to navigate to the pages of the application and added a setActivePage method to highlight the current page selected on the sidebar

--- src/app/shared/sidebar/sidebar.ts
+++ src/app/shared/sidebar/sidebar.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
-export class Sidebar {}
+export class Sidebar {
+  private router = inject(Router);
+
+  activePage = signal<string>('customers');
+
+  navigateToSelectedPage(selectedPage: string) {
+    this.router.navigate(['/', selectedPage]);
+    this.setActivePage(selectedPage);
+  }
+
+  setActivePage(newActivePage: string) {
+    this.activePage.set(newActivePage);
+  }
+}
