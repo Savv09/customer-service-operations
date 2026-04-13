7defb1107249950a3678b79f74f07f5757cc90a9
Savio <saverio.centola18@gmail.com>
2026-04-13 13:21:11 +0200

    implemented logic to fetch Customers from service adn implemented initial table to display fetched items

--- src/app/features/customers/customers.ts
+++ src/app/features/customers/customers.ts
@@ -1,17 +1,37 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 
 import { TitleService } from '../../core/services/title.service';
+import { CustomerService } from '../../core/services/customer.service';
+
+import { MatTableModule } from '@angular/material/table';
+import { Customer } from '../../core/models/customer.model';
+import { MatIcon } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-customers',
-  imports: [],
+  imports: [MatTableModule, MatIcon, MatButtonModule],
   templateUrl: './customers.html',
   styleUrl: './customers.css',
 })
 export class Customers implements OnInit {
+  customerList = signal<Customer[]>([]);
+
+  columnToDisplay = ['name', 'email', 'company', 'edit'];
+
   private titleService = inject(TitleService);
+  private customerService = inject(CustomerService);
 
   ngOnInit(): void {
     this.titleService.setTitle('Customers');
+    this.getCustomers();
+  }
+
+  getCustomers() {
+    this.customerService.getCustomerList().subscribe((res) => this.customerList.set(res));
+  }
+
+  getFullName(firstName: string, lastName: string) {
+    return `${firstName} ${lastName}`;
   }
 }
