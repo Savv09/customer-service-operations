9ec570d89e181486b732cb8510df68c18d850303
Savio <saverio.centola18@gmail.com>
2026-04-13 13:19:30 +0200

    implemented formatter to transform apiResponce in a better format

--- src/app/core/utils/api-formatter.ts
+++ src/app/core/utils/api-formatter.ts
@@ -1,4 +1,5 @@
-import { UserFromApi } from '../models/responses-from-api.model';
+import { Customer } from '../models/customer.model';
+import { CustomerFromApi, UserFromApi } from '../models/responses-from-api.model';
 import { User } from '../models/user.model';
 
 export function formatUserFromFirestore(userFromApi: UserFromApi): User {
@@ -8,5 +9,17 @@ export function formatUserFromFirestore(userFromApi: UserFromApi): User {
     lastName: userFromApi.fields.lastName?.stringValue || '',
     role: userFromApi.fields.Role?.integerValue ? Number(userFromApi.fields.Role.integerValue) : 0,
     createdAt: new Date(userFromApi.createTime),
+  };
+}
+
+export function formatCustomerFromFirestore(customerFromApi: CustomerFromApi): Customer {
+  return {
+    firstName: customerFromApi.fields.firstName.stringValue,
+    lastName: customerFromApi.fields.lastName.stringValue,
+    email: customerFromApi.fields.email.stringValue,
+    company: customerFromApi.fields.company?.stringValue || undefined,
+    phone: customerFromApi.fields.phone?.stringValue || undefined,
+    createdBy: customerFromApi.fields.createdBy.stringValue,
+    createdAt: new Date(customerFromApi.createTime),
   };
 }
