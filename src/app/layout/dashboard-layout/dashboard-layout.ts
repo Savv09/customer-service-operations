4957d588246c54949c523f60d8db9a815301da7d
Savio <saverio.centola18@gmail.com>
2026-05-04 10:16:37 +0200

    created a section with the add customer button

--- src/app/layout/dashboard-layout/dashboard-layout.ts
+++ src/app/layout/dashboard-layout/dashboard-layout.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 
 import { Sidebar } from '../../shared/sidebar/sidebar';
 import { Header } from '../../shared/header/header';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-dashborad-layout',
-  imports: [Sidebar, Header, RouterOutlet],
+  imports: [Sidebar, Header, RouterOutlet, MatIconModule],
   templateUrl: './dashboard-layout.html',
   styleUrl: './dashboard-layout.css',
 })
