67b80727cc379522c5786dcbd6540cc8be622e8e
Savio <saverio.centola18@gmail.com>
2026-05-05 11:06:08 +0200

    improved the logic to determine what title should be rendered and added the possibility to check if the title has the back button to navigate back from detail page

--- src/app/core/services/title.service.ts
+++ src/app/core/services/title.service.ts
@@ -4,9 +4,31 @@ import { Injectable, signal } from '@angular/core';
   providedIn: 'root',
 })
 export class TitleService {
-  title = signal<string>('Customer Service Operation');
+  private title = signal<string>('Customer Service Operation');
+  private hasBackButton = signal<boolean>(false);
+  private backButtonUrl = signal<string[]>([]);
+
+  getTitle() {
+    return this.title;
+  }
 
   setTitle(newTitle: string) {
     this.title.set(newTitle);
+  }
+
+  getHasBackButton() {
+    return this.hasBackButton;
+  }
+
+  setHasBackButton(newState: boolean) {
+    this.hasBackButton.set(newState);
+  }
+
+  getBackButtonUrl() {
+    return this.backButtonUrl;
+  }
+
+  setBackButtonUrl(url: string[]) {
+    this.backButtonUrl.set(url);
   }
 }
