39633fa0a93ec53fcd67140a03eefa1bf2988905
Savio <saverio.centola18@gmail.com>
2026-05-20 12:35:14 +0200

    created a role guard to allow only specific roles in ceratin routes

---
+++ src/app/core/guards/role-guard.ts
@@ -0,0 +1,24 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { UserService } from '../services/user.service';
+import { UserRole } from '../../shared/enums/user-roles.enum';
+
+export const roleGuard: CanActivateFn = (route, state) => {
+  const userService = inject(UserService);
+  const router = inject(Router);
+
+  const currentUser = userService.user();
+
+  const allowedRoles = route.data?.['roles'] as UserRole[];
+
+  if (!currentUser) {
+    router.navigate(['/', 'login']);
+  } else {
+    if (!allowedRoles.includes(currentUser?.role)) {
+      router.navigate(['/']);
+      return false;
+    }
+  }
+
+  return true;
+};
