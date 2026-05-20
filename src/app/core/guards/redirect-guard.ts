7da6084fb26766d5251a1c8d50365183a820333e
Savio <saverio.centola18@gmail.com>
2026-05-20 12:36:05 +0200

    implemented redirect guard to redirect a user after login based on their role

---
+++ src/app/core/guards/redirect-guard.ts
@@ -0,0 +1,31 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { UserService } from '../services/user.service';
+import { UserRole } from '../../shared/enums/user-roles.enum';
+
+export const redirectGuard: CanActivateFn = (route, state) => {
+  const userService = inject(UserService);
+  const router = inject(Router);
+
+  const currentUser = userService.user();
+
+  if (!currentUser) {
+    return router.navigate(['/', 'login']);
+  }
+
+  switch (currentUser.role) {
+    case UserRole.ADMIN:
+      return router.navigate(['/', 'customers']);
+
+    case UserRole.MANAGER:
+      return router.navigate(['/', 'customers']);
+
+    case UserRole.CUSTOMER:
+      return router.navigate(['/', 'tickets']);
+
+    default:
+      return router.navigate(['/', 'login']);
+  }
+
+  return true;
+};
