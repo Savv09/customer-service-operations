eecc68e6babaa1e7dd1837654258d15d10036c2e
Savio <saverio.centola18@gmail.com>
2026-05-20 12:42:11 +0200

    now menu button are visible based on a role check. Changed the activeRoute logic, now active route is obtained by subscribing to the route itself

--- src/app/shared/components/sidebar/sidebar.ts
+++ src/app/shared/components/sidebar/sidebar.ts
@@ -1,6 +1,10 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { UserRole } from '../../enums/user-roles.enum';
+import { User } from '../../../core/models/user.model';
+import { map } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-sidebar',
@@ -9,16 +13,18 @@ import { Router } from '@angular/router';
   styleUrl: './sidebar.css',
 })
 export class Sidebar {
+  currentUser = input<User | null>(null);
+
+  userRole = UserRole;
+
   private router = inject(Router);
 
-  activePage = signal<string>('customers');
+  activePage = toSignal(
+    this.router.events.pipe(map(() => this.router.url.split('/')[1] || 'customers')),
+    { initialValue: 'customers' },
+  );
 
   navigateToSelectedPage(selectedPage: string) {
     this.router.navigate(['/', selectedPage]);
-    this.setActivePage(selectedPage);
-  }
-
-  setActivePage(newActivePage: string) {
-    this.activePage.set(newActivePage);
   }
 }
