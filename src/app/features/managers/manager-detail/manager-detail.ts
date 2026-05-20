f31e63c1b3952be9debc7a38ca48763a7b6fe6f0
Savio <saverio.centola18@gmail.com>
2026-05-20 12:32:21 +0200

    changed users files in managers and updated imports and files name

---
+++ src/app/features/managers/manager-detail/manager-detail.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manager-detail',
+  imports: [],
+  templateUrl: './manager-detail.html',
+  styleUrl: './manager-detail.css',
+})
+export class ManagerDetail {}
--- src/app/features/managers/managers.ts
+++ src/app/features/managers/managers.ts
@@ -3,15 +3,15 @@ import { Component, inject } from '@angular/core';
 import { TitleService } from '../../core/services/title.service';
 
 @Component({
-  selector: 'app-users',
+  selector: 'app-managers',
   imports: [],
-  templateUrl: './users.html',
-  styleUrl: './users.css',
+  templateUrl: './managers.html',
+  styleUrl: './managers.css',
 })
-export class Users {
+export class Managers {
   private titleService = inject(TitleService);
 
   ngOnInit(): void {
-    this.titleService.setTitle('Users');
+    this.titleService.setTitle('Managers');
   }
 }
