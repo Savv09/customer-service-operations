2e23e24dd9f15a8d0768f4879e95f85163f84618
Savio <saverio.centola18@gmail.com>
2026-06-03 15:56:39 +0200

    created base code for the admin visualization of the tickets page

--- src/app/features/tickets/tickets.ts
+++ src/app/features/tickets/tickets.ts
@@ -4,20 +4,36 @@ import { TitleService } from '../../core/services/title.service';
 import { Ticket } from '../../core/models/ticket.model';
 import { UserService } from '../../core/services/user.service';
 import { UserRole } from '../../shared/enums/user-roles.enum';
+import { MatTabsModule } from '@angular/material/tabs';
+import { TicketService } from '../../core/services/ticket.service';
+import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { MatIcon } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tickets',
-  imports: [],
+  imports: [MatTabsModule, MatTableModule, MatIcon, MatButtonModule, CommonModule, ScrollingModule],
   templateUrl: './tickets.html',
   styleUrl: './tickets.css',
 })
 export class Tickets {
   private userService = inject(UserService);
   private titleService = inject(TitleService);
+  private ticketService = inject(TicketService);
+  private router = inject(Router);
 
-  user = this.userService.user;
+  currentUser = this.userService.user;
+  userRole = UserRole;
 
   ticketList = signal<Ticket[]>([]);
+  departmentList = computed(() => {
+    const departments = [...new Set(this.ticketList().map((ticket) => ticket.department))];
+
+    return departments;
+  });
 
   filter = signal<string>('');
   filteredData = computed(() => {
@@ -28,14 +44,28 @@ export class Tickets {
     return this.ticketList().filter((ticket) => ticket.title.toLowerCase().includes(filter));
   });
 
-  columnToDisplay = ['title', 'createdBy', 'department', 'edit'];
+  columnToDisplay = ['code', 'createdBy', 'assignedTo', 'status', 'priority'];
 
   ngOnInit(): void {
     this.setTitleByUser();
+    this.titleService.setHasBackButton(false);
   }
 
   setTitleByUser() {
-    const userRole = this.user()?.role;
+    const userRole = this.currentUser()?.role;
     this.titleService.setTitle(userRole === UserRole.CUSTOMER ? 'My Tickets' : 'Tickets');
+  }
+
+  getTickets() {
+    this.ticketService.getTicketList();
+    this.ticketList = this.ticketService.getTicketList$();
+  }
+
+  navigateToCreateTicket() {
+    this.router.navigate(['/', 'tickets', 'new']);
+  }
+
+  navigateToDetails(ticket: Ticket) {
+    this.router.navigate(['/', 'tickets', ticket.id]);
   }
 }
