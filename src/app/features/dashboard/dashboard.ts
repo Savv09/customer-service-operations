6dc0fb39a02e6c9220202a0ccf48ab00024fe557
Savio <saverio.centola18@gmail.com>
2026-06-03 15:55:38 +0200

    created dashboard page - still incomplete

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,5 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { TitleService } from '../../core/services/title.service';
+import { UserService } from '../../core/services/user.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,6 +10,9 @@ import { TitleService } from '../../core/services/title.service';
 })
 export class Dashboard implements OnInit {
   private titleService = inject(TitleService);
+  private userService = inject(UserService);
+
+  currentUser = this.userService.user;
 
   ngOnInit(): void {
     this.titleService.setTitle('Dashboard');
