d6f8557850b9efaec01c58f3b6cfb989bc4fca6a
Savio <saverio.centola18@gmail.com>
2026-06-08 10:49:18 +0200

    now evrey list it's being loaded right after login. Added a page of loading that the user can see during all the calls before landing on the dashboard page. Handled forkjoin error.

--- src/app/layout/dashboard-layout/dashboard-layout.ts
+++ src/app/layout/dashboard-layout/dashboard-layout.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 
 import { Sidebar } from '../../shared/components/sidebar/sidebar';
@@ -8,20 +8,60 @@ import { UserService } from '../../core/services/user.service';
 import { AuthService } from '../../core/auth/auth.service';
 import { ConfirmDialog } from '../../shared/components/confirm-dialog/confirm-dialog';
 import { MatDialog } from '@angular/material/dialog';
+import { CommonModule } from '@angular/common';
+import { forkJoin } from 'rxjs';
+import { ManagerService } from '../../core/services/manager.service';
+import { CustomerService } from '../../core/services/customer.service';
+import { TicketService } from '../../core/services/ticket.service';
 
 @Component({
   selector: 'app-dashborad-layout',
-  imports: [Sidebar, Header, RouterOutlet, MatIconModule],
+  imports: [Sidebar, Header, RouterOutlet, MatIconModule, CommonModule],
   templateUrl: './dashboard-layout.html',
   styleUrl: './dashboard-layout.css',
 })
-export class DashboradLayout {
+export class DashboradLayout implements OnInit {
+  private router = inject(Router);
+
   private userService = inject(UserService);
   private authService = inject(AuthService);
-  private router = inject(Router);
+  private managerService = inject(ManagerService);
+  private customerService = inject(CustomerService);
+  private ticketService = inject(TicketService);
+
   private dialog = inject(MatDialog);
 
-  user = this.userService.user;
+  isLoading = signal<boolean>(false);
+  isInitApplicationFailed = signal<boolean>(false);
+
+  currentUser = this.userService.getCurrentUser$();
+
+  ngOnInit(): void {
+    this.initApplication();
+  }
+
+  initApplication() {
+    this.isLoading.set(true);
+
+    forkJoin({
+      managers: this.managerService.getManagerList(),
+      customers: this.customerService.getCustomerList(),
+      // tickets: this.ticketService.getTicketList(),
+    }).subscribe({
+      next: ({ managers, customers }) => {
+        this.isInitApplicationFailed.set(false);
+
+        this.managerService.updateManagerList$(managers);
+        this.customerService.updateCustomerList$(customers);
+        // this.ticketService.updateTicketList$(tickets);
+
+        this.isLoading.set(false);
+      },
+      error: () => {
+        this.isInitApplicationFailed.set(true);
+      },
+    });
+  }
 
   onLogout() {
     const dialogRef = this.dialog.open(ConfirmDialog, {
