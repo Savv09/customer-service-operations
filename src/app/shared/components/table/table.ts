b095814f690578a764eb7f151611c6b1f7973ead
Savio <saverio.centola18@gmail.com>
2026-06-18 16:32:18 +0200

    added action column onthe table component to claim, change status and close the ticket without navigating to the details page

--- src/app/shared/components/table/table.ts
+++ src/app/shared/components/table/table.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, inject, input, output, ViewChild } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { ManagerPipe } from '../../pipes/manager-pipe';
 import { CommonModule } from '@angular/common';
@@ -10,6 +10,12 @@ import { Ticket } from '../../../core/models/ticket.model';
 import { Customer, Manager } from '../../../core/models/user.model';
 import { TableEvent } from '../../../core/models/table-event.model';
 import { ScrollingModule } from '@angular/cdk/scrolling';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { UserService } from '../../../core/services/user.service';
+import { MatMenuModule, MatMenuTrigger } from '@angular/material/menu';
+import { UserRole } from '../../enums/user-roles.enum';
 
 @Component({
   selector: 'app-table',
@@ -21,18 +27,34 @@ import { ScrollingModule } from '@angular/cdk/scrolling';
     TicketStatusPipe,
     CustomerPipe,
     ScrollingModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatMenuModule,
   ],
   templateUrl: './table.html',
   styleUrl: './table.css',
 })
 export class Table {
+  @ViewChild(MatMenuTrigger) statusMenu!: MatMenuTrigger;
+
   displayedColumns = input<string[]>([]);
   dataSource = input<any>();
 
   rowSelected = output<Ticket | Manager | Customer>();
+  ticketClaimed = output<Ticket>();
+  ticketStatusChanged = output<{ ticket: Ticket; status: TicketStatus }>();
+  ticketClosed = output<Ticket>();
+
+  private userService = inject(UserService);
+
+  currentUser = this.userService.getCurrentUser$();
+  userRole = UserRole;
 
   ticketPriority = TicketPriority;
+
   ticketStatus = TicketStatus;
+  statusOptions = Object.values(this.ticketStatus).filter((value) => typeof value === 'number');
 
   getResolutionTime(ticket: Ticket): string {
     if (!ticket.closedAt) return '-';
@@ -48,5 +70,19 @@ export class Table {
 
   onSelectRow(row: TableEvent) {
     this.rowSelected.emit(row);
+  }
+
+  onClaimTicket(ticket: Ticket) {
+    this.ticketClaimed.emit(ticket);
+  }
+
+  onChangeStatus(ticket: Ticket, status: TicketStatus, trigger: MatMenuTrigger) {
+    trigger.closeMenu();
+    this.ticketStatusChanged.emit({ ticket, status });
+    this.statusMenu.closeMenu();
+  }
+
+  onCloseTicket(ticket: Ticket) {
+    this.ticketClosed.emit(ticket);
   }
 }
